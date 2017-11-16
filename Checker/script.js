8d5933cb643198aef0e49a8fb24328f4b0344275
Kyle Coffey <kylancoffey63@gmail.com>
2017-11-16 00:23:59 -0700

    Add files via upload

---
+++ Checker/script.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="checker.html">
+Your Age: <input type="text" age="Your Name" value=""><br>
+<input type="submit" value="Submit">
+</form>
+
+if (Age < 18)
+{
+	<p1>
+	<form action="checker.html">
+	/<p1>
+}
+
+<p>Click the "Submit" button to see if you are retarded.</p>
+
+</body>
